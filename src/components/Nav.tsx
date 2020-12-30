22419d09071c5d10867e18af8a309e187adc2d69
smy-101 <shimy1011@foxmail.com>
2020-12-30 21:02:06 +0800

    使用svg-sprite-loader引入SVG文件，修改导航栏CSS样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg');
+require('icons/tags.svg');
+require('icons/statistics.svg');
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -10,7 +14,14 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 4px 0;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `
@@ -19,12 +30,21 @@ const Nav = () =>{
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref={"#tags"}/>
+                    </svg>
                     <Link to="/tags">标签</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref={"#money"}/>
+                    </svg>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref={"#statistics"}/>
+                    </svg>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
