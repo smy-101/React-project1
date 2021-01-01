aaa17ea155e9ff67a35aac895258437bffd1e249
smy-101 <shimy1011@foxmail.com>
2021-01-01 10:39:39 +0800

    修复导航栏点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,44 +4,53 @@ import React from 'react';
 import {Icon} from './Icon';
 
 
-
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
     display: flex;
+
     > li {
       width: 33.3333%;
       text-align: center;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: 4px 0;
-      .icon{
-        width: 24px;
-        height: 24px;
+      >a{
+        display: flex;
+        flex-direction: column;
+        padding: 4px 0;
+        align-items: center;
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
       }
     }
   }
-`
-const Nav = () =>{
+`;
+const Nav = () => {
     return (
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name="tags"/>
-                    <Link to="/tags">标签</Link>
+                    <Link to="/tags">
+                        <Icon name="tags"/>
+                        标签
+                    </Link>
                 </li>
                 <li>
-                    <Icon name="money"/>
-                    <Link to="/money">记账</Link>
+                    <Link to="/money">
+                        <Icon name="money"/>
+                        记账
+                    </Link>
                 </li>
                 <li>
-                    <Icon name="statistics"/>
-                    <Link to="/statistics">统计</Link>
+                    <Link to="/statistics">
+                        <Icon name="statistics"/>
+                        统计
+                    </Link>
                 </li>
             </ul>
         </NavWrapper>
-    )
-}
-export {Nav}
+    );
+};
+export {Nav};
